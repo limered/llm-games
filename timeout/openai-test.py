c9e7af2a460bfbc927ceef51926e70c11c845fd3
Emil Bohleber <emil.bohleber@brickmakers.de>
2024-03-12 17:50:39 +0100

    use jupyter notebooks

--- timeout/openai-test.py
+++ timeout/openai-test.py
@@ -1,12 +1,16 @@
-from openai import OpenAI
-client = OpenAI()
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+output_parser = StrOutputParser()
 
-print(completion.choices[0].message)
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are world class technical documentation writer."),
+    ("user", "{input}")
+])
+
+llm = ChatOpenAI()
+
+chain = prompt | llm | output_parser
+
+print(chain.invoke({"input": "how can langsmith help with testing?"}))
