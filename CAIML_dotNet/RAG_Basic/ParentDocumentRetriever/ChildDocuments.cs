7329e30fe06ce5516061d4ad05707c59fd85c737
Emil Bohleber <me@limered.de>
2024-07-12 18:01:10 +0200

    extract child docs class

---
+++ CAIML_dotNet/RAG_Basic/ParentDocumentRetriever/ChildDocuments.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using LangChain.DocumentLoaders;
+using LangChain.Extensions;
+using LangChain.Splitters.Text;
+using Microsoft.Data.Sqlite;
+
+namespace ParentDocumentRetriever;
+
+public static class ChildDocuments
+{
+    public static async Task<List<Document>> ExtractFromParentCollection(
+        string dbName,
+        string parentTableName,
+        ITextSplitter childSplitter
+    )
+    {
+        await using var connection = new SqliteConnection($"Data Source={dbName}");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = $"SELECT * FROM {parentTableName}";
+
+        var reader = await command.ExecuteReaderAsync().ConfigureAwait(false);
+
+        var retrievedParentVectors = new List<(string, string)>();
+        while (await reader.ReadAsync().ConfigureAwait(false))
+        {
+            var id = reader.GetString(0);
+            var doc = await reader.GetFieldValueAsync<string>(2).ConfigureAwait(false);
+            var document = JsonSerializer.Deserialize<Document>(doc) ?? Document.Empty;
+            var text = document.PageContent;
+
+            retrievedParentVectors.Add((id, text));
+        }
+
+        connection.Close();
+
+        return retrievedParentVectors
+            .Select(tuple => childSplitter
+                .CreateDocuments([tuple.Item2], [new Dictionary<string, object> { { "parentId", tuple.Item1 } }]))
+            .Aggregate(new List<Document>(), (list, onlyList) => list.Concat(onlyList).ToList());
+    }
+}
--- CAIML_dotNet/RAG_Basic/ParentDocumentRetriever/Program.cs
+++ CAIML_dotNet/RAG_Basic/ParentDocumentRetriever/Program.cs
@@ -1,11 +1,10 @@
-﻿using System.Text.Json;
-using Helpers;
+﻿using Helpers;
 using LangChain.Chains;
 using LangChain.Databases.Sqlite;
 using LangChain.DocumentLoaders;
 using LangChain.Extensions;
 using LangChain.Splitters.Text;
-using Microsoft.Data.Sqlite;
+using ParentDocumentRetriever;
 
 var llmModel = OpenAiModelHelper.SetupLLM();
 var embeddingModel = OpenAiModelHelper.SetupEmbedding();
@@ -14,6 +13,8 @@ const string databaseFile = "vectors.db";
 const string parentTable = "parents";
 const string childTable = "children";
 var vectorDatabase = new SqLiteVectorDatabase(databaseFile);
+await vectorDatabase.DeleteCollectionAsync(parentTable);
+await vectorDatabase.DeleteCollectionAsync(childTable);
 
 var parentCollection = await vectorDatabase.GetOrCreateCollectionAsync(parentTable, OpenAiModelHelper.Dimensions);
 var childCollection = await vectorDatabase.GetOrCreateCollectionAsync(childTable, OpenAiModelHelper.Dimensions);
@@ -23,45 +24,17 @@ var pathToFile = Path.Combine(
     Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName
         .Split(Path.DirectorySeparatorChar).Append("tesla.txt").ToArray()
 );
-
 var documents = await new FileLoader().LoadAsync(DataSource.FromPath(pathToFile));
+
 var parentSplitter = new CharacterTextSplitter(separator: " ", chunkSize: 2000, chunkOverlap: 0);
 var childSplitter = new CharacterTextSplitter(separator: " ", chunkSize: 200, chunkOverlap: 0);
 
 var splitParentDocuments = parentSplitter.SplitDocuments(documents);
-
-Console.WriteLine($"Parent Document Count: {splitParentDocuments.Count}");
-
 await parentCollection.AddDocumentsAsync(embeddingModel, splitParentDocuments);
 
 // Read Back Data
-await using var connection = new SqliteConnection($"Data Source={databaseFile}");
-connection.Open();
-
-var command = connection.CreateCommand();
-command.CommandText = $"SELECT * FROM {parentCollection.Name}";
-
-var reader = await command.ExecuteReaderAsync().ConfigureAwait(false);
-
-var retrievedParentVectors = new List<(string, string)>();
-while (await reader.ReadAsync().ConfigureAwait(false))
-{
-    var id = reader.GetString(0);
-    var doc = await reader.GetFieldValueAsync<string>(2).ConfigureAwait(false);
-    var document = JsonSerializer.Deserialize<Document>(doc) ?? Document.Empty;
-    var text = document.PageContent;
-    
-    retrievedParentVectors.Add((id, text));
-}
-
-connection.Close();
-
-var splitChildDocuments = retrievedParentVectors
-    .Select(tuple => childSplitter.CreateDocuments([tuple.Item2], [new Dictionary<string, object>{ {"parentId", tuple.Item1} }]))
-    .Aggregate(new List<Document>(), (list, onlyList) => list.Concat(onlyList).ToList());
-
-Console.WriteLine($"Child Document Count: {splitChildDocuments.Count}");
-
+var splitChildDocuments = await ChildDocuments
+        .ExtractFromParentCollection(databaseFile, parentCollection.Name, childSplitter);
 await childCollection.AddDocumentsAsync(embeddingModel, splitChildDocuments);
 
 ///////////
@@ -69,8 +42,8 @@ await childCollection.AddDocumentsAsync(embeddingModel, splitChildDocuments);
 ///////////
 
 // query
-const string question =
-    "What was the reason for Tesla not to mary?";
+const bool useParent = true;
+const string question = "What was the reason for Tesla not to mary?";
 var similarDocuments = await childCollection.GetSimilarDocuments(
     embeddingModel: embeddingModel,
     request: question,
@@ -78,10 +51,13 @@ var similarDocuments = await childCollection.GetSimilarDocuments(
 
 var result = similarDocuments.AsString();
 
-if(similarDocuments.Count != 0)
+if(useParent && similarDocuments.Count != 0)
 {
     result = (await parentCollection.GetAsync(similarDocuments.First().Metadata["parentId"].ToString()!))!.Text;
 }
+
+Console.WriteLine($"Parent Document Count: {splitParentDocuments.Count}");
+Console.WriteLine($"Child Document Count: {splitChildDocuments.Count}");
 
 Console.WriteLine("Child: ");
 Console.WriteLine(similarDocuments.AsString());
@@ -92,7 +68,8 @@ Console.WriteLine(result);
 var prompt = $"""
               Use only the following pieces of context to answer the question at the end.
               If the answer is not in context then just say that you don't know, don't try to make up an answer.
-
+              Give as much detail as possible without inventing or using learned knowledge.
+              
               << Context >>
               {result}
               << Context End >>
